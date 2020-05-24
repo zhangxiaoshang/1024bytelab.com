52b34ec694cc47e3b5479d40f811a1a7d065b1c8
zhangxiaoshang <zhangxiaoshang66@163.com>
2020-05-24 20:55:28 +0800

    update docs

--- docs/umi.js
+++ docs/umi.js
@@ -49,7 +49,7 @@
     (n.o = function(e, t) {
       return Object.prototype.hasOwnProperty.call(e, t);
     }),
-    (n.p = 'https://zhangxiaoshang.github.io/1024bytelab.com/'),
+    (n.p = 'https://1024bytelab.com/'),
     n((n.s = 0));
 })({
   '++zV': function(e, t, n) {
