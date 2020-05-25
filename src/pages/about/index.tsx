2f4fa914e7fb9df0de193b032d7b11311500ff6e
zhangxiaoshang <zhangxiaoshang66@163.com>
2020-05-25 19:14:46 +0800

    temp

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.less';
+
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>关于1024字节实验室</h1>
+      <p>这是一个个人项目，和拉勾、掘金等平台无关</p>
+      <h2>一句话介绍本站</h2>
+      <p>
+        通过本站跳转到拉勾、掘金平台购买课程、小册，可以联系我领取奖励红包。
+      </p>
+      <h2>运行原理</h2>
+    </div>
+  );
+};
